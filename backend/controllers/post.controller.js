cc9473e6ef39c30b57cdb2e6cad27176bb41ebba
Justl3est <111554501+justbest2003@users.noreply.github.com>
2025-01-08 08:32:12 +0700

    Update post.controller.js

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,4 +1,4 @@
-const PostModel = require("../models/Post");
+const PostModel = require("../models/Posttest");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const secret = process.env.SECRET;
